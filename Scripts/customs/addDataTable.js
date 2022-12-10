63e9bbb06a403d3fb4699ab08363fc6f604eb147
Hoàng Nam <nnam44565@gmail.com>
2022-12-10 18:43:56 +0700

    add mysql

--- Scripts/customs/addDataTable.js
+++ Scripts/customs/addDataTable.js
@@ -3,5 +3,17 @@ formTable.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(formTable);
   const data = Object.fromEntries(formData);
-  console.log(data);
+  fetch('http://localhost:3001/total-earning', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.code === 1) {
+        alert('Thêm thành công');
+      }
+    });
 });
