99155db74d2f3b21351e3f88a493d9ff69932647
Hoàng Nam <nnam44565@gmail.com>
2022-12-18 16:48:27 +0700

    delete

--- Scripts/customs/index.js
+++ Scripts/customs/index.js
@@ -8,6 +8,7 @@ const renderTable = (data) => {
   const html =
     data?.length > 0 &&
     data?.map((item, index) => {
+      console.log(item?.Employee_ID);
       return `
       <tr class="odd gradeX">
       <td>${index + 1 || ''}</td>
@@ -20,10 +21,26 @@ const renderTable = (data) => {
       <td>${item?.Vacation_Days || ''}</td>
       <td>${item?.Paid_To_Date || ''}</td>
       <td>${item?.Paid_Last_Year || ''}</td>
+      <td>
+        <a  
+         onclick="editEmployee(${item?.Employee_ID})"
+        class="btn btn-primary btn-sm">Edit</a>
+        <a
+        onclick="deleteEmployee(${item?.Employee_ID})"
+        class="btn btn-danger btn-sm">Delete</a>
+      </td>
   </tr>
     `;
     });
   return html?.join('');
+};
+const deleteEmployee = async (id) => {
+  const res = await fetch(`http://localhost:3001/total-earning/${id}`, {
+    method: 'DELETE',
+  });
+  const data = await res.json();
+  alert('Delete success');
+  window.location.reload();
 };
 
 const main = async () => {
